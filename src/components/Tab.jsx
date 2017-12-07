e9196b532458c3e4867dc1bb0fa2e9b60e4bd0c9
AlecksJohannes <alecksjohanssen@gmail.com>
2017-12-07 11:47:57 +0700

    Bad indentation code

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 export default class Tab extends React.Component {
-	render() {
-		return(
-			<div className={this.props.isActive ? 'selected' : 'a'} onClick={(element) => {this.props.onSetActiveTab(element.currentTarget.className)}}>
-				<h1>{this.props.tabName}</h1>
-			</div>
-		)
-	}
+  render() {
+    return(
+      <div className={this.props.isActive ? 'selected' : 'a'} onClick={(element) => {this.props.onSetActiveTab(element.currentTarget.className)}}>
+        <h1>{this.props.tabName}</h1>
+      </div>
+    )
+  }
 }
 
